8b356937f025d3881818cd624368b89be8372ecb
DavisSnow002 <nieveshernandezjesusdavid@gmail.com>
2022-05-22 21:55:02 -0500

    Primer commit v2

---
+++ app/src/main/java/com/example/myapplicationkotlin/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.myapplicationkotlin.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFE53935)
+val Purple500 = Color(0xFFFF6F60)
+val Purple700 = Color(0xFFAB000D)
+val Teal200 = Color(0xFF03DAC5)
